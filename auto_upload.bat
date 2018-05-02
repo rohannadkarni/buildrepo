754ea40a35734f18999d9b55a2c9202322e9bff6
rohannadkarni <37760203+rohannadkarni@users.noreply.github.com>
2018-05-02 11:34:16 -0400

    uploading at 11:34:16.02

--- auto_upload.bat
+++ auto_upload.bat
@@ -1,4 +1,3 @@
-@ECHO OFF
 
 SET curr_path="C:\Users\Rohan\eclipse-workspace\maven-demo"
 SET repo_url="https://github.com/rohannadkarni/buildrepo.git"
@@ -7,6 +6,8 @@ ECHO We're working with "%curr_path%"
 ECHO We're working with "%repo_url%"
 
 pause;
+
+ECHO OFF
 
 call "C:\Program Files\Git\bin\git.exe" init
 call "C:\Program Files\Git\bin\git.exe" add %curr_path%
@@ -18,4 +19,4 @@ call "C:\Program Files\Git\bin\git.exe" remote add buildrepo %repo_url%
 
 call "C:\Program Files\Git\bin\git.exe" push buildrepo master
 
-pause;
+start "" %repo_url%
